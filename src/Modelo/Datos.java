651154f50fdfc08410b1e99af56c0fc1b2d4db92
Daniel Cuque <danielcuque78@gmail.com>
2022-03-09 23:04:58 -0600

    Configuración del gesto de archivos

--- src/Modelo/Datos.java
+++ src/Modelo/Datos.java
@@ -11,5 +11,33 @@ package Modelo;
  */
 public class Datos {
     
+    private String columna1;
+    private int columna2;
+    
+    public Datos(String columna1, int columna2){
+        this.columna1 = columna1;
+        this.columna2 = columna2;
+    }
+
+    public String getColumna1() {
+        return columna1;
+    }
+
+    public void setColumna1(String columna1) {
+        this.columna1 = columna1;
+    }
+
+    public int getColumna2() {
+        return columna2;
+    }
+
+    public void setColumna2(int columna2) {
+        this.columna2 = columna2;
+    }
+    
+    
+    
+    
+    
     
 }
